dcecc64da6103d45c4f340e3c4c261e20d463743
Nelly <Nelly.sosobrado@gmail.com>
2026-09-10 12:20:14 +0200

    Created DiceResult modell

---
+++ DiceRoller/Models/DiceResult.cs
@@ -0,0 +1,12 @@
+﻿namespace DiceRoller.Models
+{
+    public class DiceResult
+    {
+        public List<int> Rolls { get; set; } = new List<int>();
+
+        public int Total { get; set; }
+        public double Average { get; set; }
+        public int Highest { get; set; }
+        public int Lowest { get; set; }
+    }
+}
