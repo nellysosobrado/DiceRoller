e2a8b7cf563105d79bf23736da14dfb59be28a11
Nelly <Nelly.sosobrado@gmail.com>
2026-09-10 12:31:16 +0200

    refactor: switch dice logic to DiceResult model and update UI

--- DiceRoller/Services/DiceService.cs
+++ DiceRoller/Services/DiceService.cs
@@ -1,10 +1,33 @@
-﻿namespace DiceRoller.Services
+﻿using DiceRoller.Models;
+
+namespace DiceRoller.Services
 {
     public class DiceService
     {
-        public int Roll()
+        public DiceResult Roll(int numberOfRolls)
         {
-            return Random.Shared.Next(1, 7);
+            List<int> rolls = new List<int>();
+
+            for(int i = 0; i< numberOfRolls;i++)
+            {
+                int diceRoll = Random.Shared.Next(1, 7);
+
+                rolls.Add(diceRoll);
+
+            }
+            DiceResult result = new DiceResult();
+
+            result.Rolls = rolls;
+            result.Total = rolls.Sum();
+            result.Average = rolls.Average();
+            result.Highest = rolls.Max();
+            result.Lowest = rolls.Min();
+
+            return result;
         }
+        //public int Roll()
+        //{
+        //    return Random.Shared.Next(1, 7);
+        //}
     }
 }
